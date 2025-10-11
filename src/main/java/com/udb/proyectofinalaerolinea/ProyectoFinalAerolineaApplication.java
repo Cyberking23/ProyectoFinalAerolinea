9c3e293e3bc9c059d119a7f7cb17fe699a27f02f
Carlos Merino <merinoventura123@gmail.com>
2025-10-10 18:28:40 -0600

    Base Proyecto
    Base Proyecto de Aerolinea

---
+++ src/main/java/com/udb/proyectofinalaerolinea/ProyectoFinalAerolineaApplication.java
@@ -0,0 +1,13 @@
+package com.udb.proyectofinalaerolinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalAerolineaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalAerolineaApplication.class, args);
+    }
+
+}
